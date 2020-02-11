266eda731aac048a72d2753444354a2a6b45db47
Harith Dilshan <harithdilshan777@gmail.com>
2020-02-11 15:28:16 +0530

    Add files via upload

---
+++ Lydia Speek/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Lydia_Speek
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Henry = new SpeechSynthesizer();
+
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"C:\Henry\com.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+    }
+}
